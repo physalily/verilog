aabf2cec36e16d7fe45e10d3aa3f460fcc6421a7
physalily <physalily@gmail.com>
2019-02-01 14:07:58 +0900

    MEN AT WORK

---
+++ activate.v
@@ -0,0 +1,11 @@
+/******************************
+made 2019/1/30 powered harumaki
+module name: activation
+this is activation function
+
+only ReLU
+*******************************/
+
+module activation();
+    
+endmodule
