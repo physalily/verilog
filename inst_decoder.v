0cf28462b099f39ff9b55fd7b8128def5f01bfbd
physalily <physalily@gmail.com>
2019-02-03 16:37:39 +0900

    first commit & create file

---
+++ inst_decoder.v
@@ -0,0 +1,10 @@
+/******************************
+made 2019/2/3 powered harumaki
+module name: inst_decoder
+this is instruction decoder
+function list
+*******************************/
+
+module inst_decoder();
+    
+endmodule
