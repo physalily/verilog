1b1ba954a0c52d9ad3d6323dfbc997ebac0ee556
physalily <physalily@gmail.com>
2019-02-03 14:16:50 +0900

    MEN AT WORK

--- CoreSocket.v
+++ CoreSocket.v
@@ -1,16 +1,27 @@
 /******************************
 made 2019/1/25 powered harumaki
 module name: CoreSocket
-this is 
-
+this is prcessing unit
 function list
 *******************************/
 
 module CoreSocket();
-
-    begin
-    MatrixCore(
-        
-        );
-    end
+    parameter DATA_SIZE = 'd16;
+    parameter COLUMN_SIZE = 'd16;
+    parameter ROW_SIZE = 'd16;
+    parameter Cores = 'd4;
+    
+    generate 
+    genvar i;
+        for(i = 0; i < Cores; i = i +1)
+        begin
+            :Core
+            MatrixCore
+            #(DATA_SIZE, COLUMN_SIZE, ROW_SIZE)
+            matrix(
+            
+            );
+        end
+    endgenerate
+    
 endmodule
